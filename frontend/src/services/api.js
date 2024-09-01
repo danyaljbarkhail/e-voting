0472b6dcd7bb15e5d8323a5eb0c0a8d7bd36f97c
Danyal Jabarkhail <149615447+danyaljbarkhail@users.noreply.github.com>
2024-09-02 01:02:48 +0500

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://e-voting-backend.onrender.com', // Ensure this matches your backend base URL
+  baseURL: 'https://e-voting-backend.onrender.com/api', // Ensure this matches your backend base URL
   headers: {
     'Content-Type': 'application/json',
   },
